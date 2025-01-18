88be45c7d59e5f13819538656b9527fce697adaf
dinesh-thurpati <dinesheshan.852@gmail.com>
2025-01-18 13:29:21 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,23 @@
+find difference between null and undefined
+-- null does not assign any value and undefined means it has no initialization
+
+datatyypes in js ??
+number
+String
+Boolean
+undefined
+null
+Symbol
+Object
+
+
+typeof in js?
+what type of value the variable assigned in browser.
+
+
+difference between == and === ??
+
+== is checks value 
+=== is checks both value and data type
+
+
